ff8ff4da65d3f86081d6c52d642ac2d119eac5d5
maulik <maulikbeladiya01@gmail.com>
2020-11-02 17:14:01 +0530

    functionality is done UI changes is uncomplete

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, CreateView
 from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
+from django.core.files import File
 
 from .forms import BookForm
 from .models import Book
@@ -66,57 +67,19 @@ def upload(request):
         graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
         graph.write_png(image_url)
         imge = Image(graph.create_png())
+
         
         fs = FileSystemStorage()
         upload_file_name = uploaded_file
-        name = fs.save(uploaded_file.name, uploaded_file)
         path_to_generated_image = os.path.abspath(os.path.join(os.getcwd(), os.pardir))+"\\django-upload-example\\"+ image_url
-        print(path_to_generated_image)
-        context['url'] = path_to_generated_image
-        
+        file = open(path_to_generated_image , "rb")
+        django_file = File(file)
 
-    #fileUrl = os.path.join(BASE_DIR , '\\media\\' + upload_file_name)
-    #
-    #print(type(image))
+        name_of_image = "Tree_of_"+str(os.path.splitext(uploaded_file.name)[0]) + ".png"
+        name = fs.save(name_of_image, django_file)
+
+        print(path_to_generated_image)
+        context['image_name'] = name_of_image 
     return render(request, 'upload.html', context)
 
 
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'book_list.html', {
-        'books': books
-    })
-
-
-
-def upload_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'upload_book.html', {
-        'form': form
-    })
-
-
-def delete_book(request, pk):
-    if request.method == 'POST':
-        book = Book.objects.get(pk=pk)
-        book.delete()
-    return redirect('book_list')
-
-
-class BookListView(ListView):
-    model = Book
-    template_name = 'class_book_list.html'
-    context_object_name = 'books'
-
-
-class UploadBookView(CreateView):
-    model = Book
-    form_class = BookForm
-    success_url = reverse_lazy('class_book_list')
-    template_name = 'upload_book.html'
